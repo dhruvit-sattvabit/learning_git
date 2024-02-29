dcec22f72ab695ac61145d142ef40bbee423cdf5
Dhruvit Sattvabit <Dhruvit@Sattvabit.com>
2024-02-29 12:41:11 +0530

    second

---
+++ learngit/index.js
@@ -0,0 +1,11 @@
+const age = 24;
+const name1 = 'Dhruvit Mistry'
+const dob = '14/09/2002'
+
+function sayName() {
+  console.log('My Name is ' + name1)
+}
+
+function isAbove18() {
+  return age >= 18
+}
